b572fe5074ec02fc143736c57c5494976418cd84
Ando Deng <87212291+AD1938@users.noreply.github.com>
2022-12-02 19:30:35 -0500

    add, delete

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Members
 # Create your views here.
 
@@ -13,3 +14,18 @@ def index(request):
 
   return HttpResponse(template.render(context, request))
 
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Members(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+  member = Members.objects.get(id=id)
+  member.delete()
+  return HttpResponseRedirect(reverse('index'))
